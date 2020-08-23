01abbf69089b3a5de12d5597c6574e00e792781d
nargok <nargok@gmail.com>
2020-08-24 08:48:47 +0900

    primitiveStore

--- src/views/Count.vue
+++ src/views/Count.vue
@@ -2,11 +2,14 @@
   <div>
     <Counter name='Counter 1' :initCount="5" @emitUp="getEvent" />
     <Counter name='Counter 2' :initCount="10" @emitUp="getEvent" />
+    <p>{{ primitiveStore.$data.globalCount }}</p>
+    <input type="text" v-model="primitiveStore.$data.globalCount">
   </div>
 </template>
 
 <script>
 import Counter from '@/components/Counter.vue'
+import primitiveStore from '@/primitiveStore.js'
 
 export default {
   components: {
@@ -14,12 +17,12 @@ export default {
   },
   data() {
     return {
-      stack: [],
+      primitiveStore,
     }
   },
   methods: {
-    getEvent(payload) {
-      this.stack.push(payload);
+    getEvent() {
+      this.primitiveStore.$data.globalCount++;
     }
   }
 }
