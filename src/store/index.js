7d2cb7e86bb8babf08a5c76e459c044e6c329fe8
nargok <nargok@gmail.com>
2020-08-24 09:24:06 +0900

    use vuex data

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    globalCount: 0,
   },
   mutations: {
+    globalIncrement(state) {
+      state.globalCount++;
+    }
   },
   actions: {
   },
--- src/views/Count.vue
+++ src/views/Count.vue
@@ -2,28 +2,32 @@
   <div>
     <Counter name='Counter 1' :initCount="5" @emitUp="getEvent" />
     <Counter name='Counter 2' :initCount="10" @emitUp="getEvent" />
-    <p>{{ primitiveStore.$data.globalCount }}</p>
-    <input type="text" v-model="primitiveStore.$data.globalCount">
-  </div>
+    <p>{{ count }}</p>
+    <p>{{ globalCount }}</p>
+   </div>
 </template>
 
 <script>
 import Counter from '@/components/Counter.vue'
-import primitiveStore from '@/primitiveStore.js'
+import { mapState } from 'vuex';
 
 export default {
   components: {
     Counter,
   },
-  data() {
-    return {
-      primitiveStore,
-    }
-  },
   methods: {
     getEvent() {
-      this.primitiveStore.$data.globalCount++;
+      this.$store.commit('globalIncrement')
+    },
+    shouldNotThis() {
+      this.$store.state.globalCount++;
     }
+  },
+  computed: {
+    count() {
+      return this.$store.state.globalCount;
+    },
+    ...mapState(["globalCount"])
   }
 }
 </script>
